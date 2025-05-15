d4ff8ea9359ec6e3f3a8bd8fbd9dc2ad0fa85d1c
Vilith <vilith44@gmail.com>
2025-05-15 14:15:40 +0200

    Added ocustom validationattribute

--- EmployeesApp.Web/Models/Employee.cs
+++ EmployeesApp.Web/Models/Employee.cs
@@ -6,9 +6,28 @@ namespace EmployeesApp.Web.Models
     {
         public int Id { get; set; }
 
+        [HardCodedString("NSFW word here")]
         [Required/*(ErrorMessage = "Enter a name")*/]
         public string Name { get; set; } = string.Empty;
+
         [EmailAddress]
         public string Email { get; set; } = string.Empty;
+    }
+
+    public class HardCodedStringAttribute : ValidationAttribute
+    {
+        private readonly string _hardCodedString;
+        public HardCodedStringAttribute(string hardCodedString)
+        {
+            _hardCodedString = hardCodedString;
+        }
+        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is string strValue && strValue == _hardCodedString)
+            {
+                return new ValidationResult($"The value '{_hardCodedString}' is not allowed.");
+            }
+            return ValidationResult.Success!;
+        }
     }
 }
